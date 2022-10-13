a065e2f2a58c785a019c1e658001b83f8b74495d
Roberto <robicarlomann@hotmail.com>
2022-10-12 19:03:32 -0600

    .

--- src/IdentityServer/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace IdentityServerAspNetIdentity.Pages.Logout;
-
-public class LogoutOptions
-{
-    public static bool ShowLogoutPrompt = false;
-    public static bool AutomaticRedirectAfterSignOut = false;
-}
